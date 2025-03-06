93740752f7bea00deba2588f3ee8d31a62a6e1f2
Sameer Wadkar <wadkarsamir32@gmail.com>
2025-03-07 00:37:56 +0530

    Solved problem on LinkedList

---
+++ src/LinkedList/LinkedListCycle.java
@@ -0,0 +1,58 @@
+package LinkedList;
+
+
+import java.util.List;
+
+class ListNode{
+    int val;
+    ListNode next;
+
+    public ListNode(int val){
+        this.val= val;
+    }
+
+    public ListNode(ListNode next){
+        this.next= next;
+    }
+
+}
+public class LinkedListCycle {
+
+    public static void main(String[] args) {
+        int[] nodes= {3,2,0,-4};
+
+        ListNode head= new ListNode(0);
+        ListNode temp= head;
+        ListNode cycleNode= null;
+
+        for(int i=1; i<nodes.length; i++){
+            temp.next= new ListNode(nodes[i]);
+            temp=temp.next;
+            if(i==1){
+                cycleNode= temp;
+            }
+        }
+        temp.next=cycleNode;
+
+        System.out.println(hasCycle(head));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+
+        if(head==null || head.next==null){
+            return false;
+        }
+
+        ListNode slow= head;
+        ListNode fast= head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
